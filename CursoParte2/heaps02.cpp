22c1936cab86609c856fa3818874d545fb42fae8
Santiago <santifuentest12@gmail.com>
2025-03-12 03:11:58 -0600

    Constuccion Max-heap. algoritmo O(n)

--- CursoParte2/heaps02.cpp
+++ CursoParte2/heaps02.cpp
@@ -1,3 +1,56 @@
-//
-// Created by santi on 18/02/2025.
-//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MaxHeap {
+private:
+  vector<int> heap;
+
+  void heapifyDown(int index) {
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    int largest = index;
+
+    if (left < heap.size() && heap[left] > heap[largest]) {
+      largest = left;
+    }
+    if (right < heap.size() && heap[right] > heap[largest]) {
+      largest = right;
+    }
+    if (largest != index) {
+      swap(heap[index], heap[largest]);
+      heapifyDown(largest);
+    }
+  }
+
+public:
+  void buildHeap(vector<int> &arr) {
+    heap = arr;
+    for (int i = (heap.size() / 2) - 1; i >= 0; i--) {
+      heapifyDown(i);
+    }
+  }
+
+  void printHeap() {
+    for (int val : heap) {
+      cout << val << " ";
+    }
+    cout << endl;
+  }
+};
+
+int main() {
+  MaxHeap h;
+  vector<int> arr = {10, 50, 30, 20, 35, 15, 40};
+
+  cout << "Array original: ";
+  for (int val : arr) cout << val << " ";
+  cout << endl;
+
+  h.buildHeap(arr);
+
+  cout << "Heap construido: ";
+  h.printHeap();
+
+  return 0;
+}
